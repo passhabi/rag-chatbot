a3d2db915c1da79a50411cc449220fe909cd6e86
Pedram Asshabi <pedramas@live.com>
2024-09-23 11:22:35 +0330

    Chunks the document.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-import pypdf
-#
-# from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+
 # from langchain_openai import OpenAIEmbeddings
 # from langchain_pinecone import PineconeVectorStore
+# import pyloader
 
 loader = PyPDFLoader("data/a_mind_for_numbers.pdf")
 document = loader.load()
+
+text_splitter = CharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=100)
+
+texts = text_splitter.split_documents(document)
+print(f"There is {len(texts)} chunks")
